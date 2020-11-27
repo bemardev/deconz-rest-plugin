222ec47604c10e913d1df0a10998cd49db231d32
Mdleal <michael.leal82@gmail.com>
2020-11-27 15:26:07 -0600

    Update de_web_plugin.cpp
    Adding ';' after break.

--- de_web_plugin.cpp
+++ de_web_plugin.cpp
@@ -877,7 +877,7 @@ void DeRestPluginPrivate::apsdeDataIndication(const deCONZ::ApsDataIndication &i
             
         case DOOR_LOCK_CLUSTER_ID:
             DBG_Printf(DBG_INFO, "Door lock debug 0x%016llX, data 0x%08X \n", ind.srcAddress().ext(), zclFrame.commandId() );
-            break
+            break;
         
 
         default:
